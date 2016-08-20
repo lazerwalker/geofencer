64c8c343b946a605036666a1539deb28c2f3334e
Mike Walker <michael@lazerwalker.com>
2016-08-20 13:34:03 -0400

    Add view bindings to VC

--- GeoFencer/ViewController.swift
+++ GeoFencer/ViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    
+    @IBAction func didTapFirstButton(sender: AnyObject) {
+    }
+    @IBAction func didTapSecondButton(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
