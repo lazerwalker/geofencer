f57d500223bef5a28e091b3d3b36666b8dfd56fc
Mike Walker <michael@lazerwalker.com>
2016-08-20 14:44:43 -0400

    Draw points on map

--- GeoFencer/ViewController.swift
+++ GeoFencer/ViewController.swift
@@ -5,14 +5,35 @@ import UIKit
 class ViewController: UIViewController {
     let locationManager = CLLocationManager()
 
+    var first:MKPointAnnotation?
+    var second:MKPointAnnotation?
+
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var secondButton: UIButton!
     
     @IBAction func didTapFirstButton(sender: AnyObject) {
-        print(mapView.userLocation.location)
+        if let first = first {
+            mapView.removeAnnotation(first)
+        }
+
+        first = MKPointAnnotation()
+        if let first = first, location = mapView.userLocation.location {
+            first.coordinate = location.coordinate
+            mapView.addAnnotation(first)
+        }
     }
+
     @IBAction func didTapSecondButton(sender: AnyObject) {
+        if let second = second {
+            mapView.removeAnnotation(second)
+        }
+
+        second = MKPointAnnotation()
+        if let second = second, location = mapView.userLocation.location {
+            second.coordinate = location.coordinate
+            mapView.addAnnotation(second)
+        }
     }
     
     override func viewDidLoad() {
