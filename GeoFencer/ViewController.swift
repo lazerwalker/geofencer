5cb73dd02f6d9ec44ebf440a6887fdc297f90626
Mike Walker <michael@lazerwalker.com>
2016-08-20 13:30:42 -0400

    Initial commit

---
+++ GeoFencer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoFencer
+//
+//  Created by Mike Lazer-Walker on 8/20/16.
+//  Copyright © 2016 Mike Lazer-Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
