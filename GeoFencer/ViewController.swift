4e62f2a2b010b449a5e623bfdd3fa4acd0c7868a
Mike Walker <michael@lazerwalker.com>
2016-08-20 13:51:51 -0400

    Fetch user location

--- GeoFencer/ViewController.swift
+++ GeoFencer/ViewController.swift
@@ -1,27 +1,26 @@
+import CoreLocation
 import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
 
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var secondButton: UIButton!
     
     @IBAction func didTapFirstButton(sender: AnyObject) {
+        print(mapView.userLocation.location)
     }
     @IBAction func didTapSecondButton(sender: AnyObject) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.FollowWithHeading, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
