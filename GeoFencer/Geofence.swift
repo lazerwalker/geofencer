928d7514bf5a43cb35dde0df006797622dc4328a
Mike Walker <michael@lazerwalker.com>
2016-08-29 12:19:51 -0700

    Write the worst JSON serializer ever

--- GeoFencer/Geofence.swift
+++ GeoFencer/Geofence.swift
@@ -22,4 +22,33 @@ class Geofence {
                                     fabs(p1.y-p2.y))
         return MKCircle(mapRect:mapRect)
     }
+
+    //-
+    // TODO: All the lols.
+    func toJSON() -> String {
+        let pointString = points.map { (coordinate) -> String in
+            return String("\"\(coordinate.latitude),\(coordinate.longitude)\"")
+            }.joinWithSeparator(",")
+        return "{\"points\":[\(pointString)]}"
+    }
+
+    class func fromJSON(json:String) -> Geofence? {
+        do {
+            let data = json.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+            let json = try NSJSONSerialization.JSONObjectWithData(data, options: []) as! [String: AnyObject]
+            if let pointStrings = json["points"] as? [String] {
+                let points = pointStrings.map({ (str) -> CLLocationCoordinate2D in
+                    let latlng = str.componentsSeparatedByString(",")
+                    let lat = (latlng[0] as NSString).doubleValue
+                    let lng = (latlng[1] as NSString).doubleValue
+                    return CLLocationCoordinate2D(latitude: lat, longitude: lng)
+                })
+                return Geofence(points: points)
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+            return nil
+        }
+        return nil
+    }
 }
